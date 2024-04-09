f7b8569c6e3f08d7bc0dd22a7e14cd02c99cf517
EduardoBontorin <eduardo.bontorin@outlook.com>
2024-04-09 08:36:52 -0300

    Adicionado Workflow

---
+++ AppState.cs
@@ -0,0 +1,17 @@
+﻿using IMC2.Models;
+
+namespace IMC2
+{
+    public static class AppState
+    {
+        public static List<ImcModel> History { get; set; } = new();
+
+        public static event EventHandler<EventArgs> OnChanged;
+
+        public static void NotifyChanges(object? sender, EventArgs args)
+        {
+            OnChanged.Invoke(sender, args);
+        }
+    }
+
+}
